b2a98ae791ec117a0f1d8ea3206a7bbd8000f009
liukailiukai <liukailiukai@liukaideMacBook-Pro.local>
2017-11-08 16:38:37 +0800

    update

--- script/start.sh
+++ script/start.sh
@@ -1,3 +1,3 @@
 CURRENTPATH=`cd \`dirname $0\`; pwd`
 cd $CURRENTPATH
-../sbin/haproxy -f ../conf/haproxy.cfg -sf `cat $CURRENTPATH/haproxy.pid`
+../sbin/haproxy -f ../conf/haproxy.cfg -p $CURRENTPATH/haproxy.pid -sf `cat $CURRENTPATH/haproxy.pid`
