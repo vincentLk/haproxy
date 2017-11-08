291f7055bcadf9f6ff166e39296a43ffd0648587
liukailiukai <liukailiukai@liukaideMacBook-Pro.local>
2017-11-08 17:10:42 +0800

    update

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,9 @@
+set -e
 CURRENTPATH=`cd \`dirname $0\`; pwd`
 cd $CURRENTPATH/../package
 tar -zxvf haproxy-1.7.9.tar.gz
+cd haproxy-1.7.9
 make TARGET=linux2628 CPU=x86_64  PREFIX=$CURRENTPATH/..
 make install PREFIX=$CURRENTPATH/..
+rm -rf haproxy-1.7.9
 
